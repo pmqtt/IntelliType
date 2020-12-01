a098f117ce1d5695e42e488f9a008312049cc228
michaelburzan <michael.burzan@outlook.de>
2020-12-01 08:22:55 +0100

    prepare setup for reading README

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='IntelliType',
@@ -14,5 +18,7 @@ setup(
         'console_scripts': [
             'itt=IntelliType:run',
         ],
-    }
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
