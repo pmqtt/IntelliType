7b4ef9e8a0cb5cda22ce85aa0d4db04b9b83a7ec
michaelburzan <michael.burzan@outlook.de>
2020-12-04 16:02:37 +0100

    use logfile

--- IntelliType/Program.py
+++ IntelliType/Program.py
@@ -8,7 +8,8 @@ def run_program_in_thread(command: str):
     is_windows = sys.platform.startswith('win')
     if is_windows:
         DETACHED_PROCESS = 0x00000008
-        subprocess.Popen(shlex.split(command), creationflags=DETACHED_PROCESS, shell=True, stdout=subprocess.DEVNULL,
+        logfile = open("logfile.tmp",'a+')
+        subprocess.Popen(shlex.split(command), creationflags=DETACHED_PROCESS, shell=True, stdout=logfile,
                          stderr=subprocess.DEVNULL)
     else:
         subprocess.Popen(shlex.split(command))
