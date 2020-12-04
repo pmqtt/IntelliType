9d5847e6177d4c9faddac7ce919752f94ccf5e7a
michaelburzan <michael.burzan@outlook.de>
2020-12-04 08:29:35 +0100

    move process to seperate thread

--- IntelliType/Program.py
+++ IntelliType/Program.py
@@ -1,21 +1,27 @@
 import sys
 import subprocess
 import shlex
+import threading
+
+def run_program_in_thread(command : str):
+    is_windows = sys.platform.startswith('win')
+    if is_windows:
+        DETACHED_PROCESS = 0x00000008
+        subprocess.Popen(shlex.split(command), creationflags=DETACHED_PROCESS, shell=True, stdout=subprocess.DEVNULL,
+                         stderr=subprocess.DEVNULL)
+    else:
+        subprocess.Popen(shlex.split(command))
+
 
 class Program:
-
     def __init__(self, command: str):
         self.command = command
         self.prepare_command()
-        self.pid = -1
-    def execute(self):
-        is_windows = sys.platform.startswith('win')
-        if is_windows:
-            DETACHED_PROCESS = 0x00000008
-            self.pid = subprocess.Popen(shlex.split(self.command),creationflags=DETACHED_PROCESS,shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        else:
-            self.pid = subprocess.Popen(shlex.split(self.command))
+        self.process = None
 
+    def execute(self):
+        thread = threading.start_new_thread(run_program_in_thread,self.command)
+        thread.detach()
 
 
     def prepare_command(self):
--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='IntelliType',
-    version='0.3.1',
+    version='0.3.3',
     packages=['IntelliType'],
     url='https://github.com/pmqtt/IntelliType',
     license='Apache 2',
     author='michael burzan',
     author_email='',
-    description='',
+    description='The goal of IntelliType is to help you automate text-based processes',
     install_requires = ['keyboard'],
     entry_points={
         'console_scripts': [
